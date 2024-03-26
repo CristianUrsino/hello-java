6c05023ae3dbb5d5ff4ebcac9f7a7df3a73ef6f1
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-03-26 12:57:22 +0100

    first commit HelloJava

---
+++ src/org/learning/basics/HelloJava.java
@@ -0,0 +1,8 @@
+package org.learning.basics;
+
+public class HelloJava{
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.print(6+4);
+    }
+}
